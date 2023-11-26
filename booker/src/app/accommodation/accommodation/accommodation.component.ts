27f6e9d39fc126589c823e65bfe20c6a585b81a9
milamilovic <milamilovic03@gmail.com>
2023-11-26 15:48:54 +0100

    image slider

--- booker/src/app/accommodation/accommodation/accommodation.component.ts
+++ booker/src/app/accommodation/accommodation/accommodation.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AccommodationComponent {
 
+  slides = [
+    {'image': "../../../assets/kitchen-2165756_640.jpg"},
+    {'image': "../../../assets/apartment_image.jpg"},
+    {'image': "../../../assets/living-room.jpg"}
+   ];
+  // slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+  //
+  // constructor() {}
+  //
+  // ngOnInit(): void {
+  //   this.slides[0] = {
+  //     src: "../../../assets/kitchen-2165756_640.jpg",
+  //   };
+  //   this.slides[1] = {
+  //     src: "../../../assets/apartment_image.jpg"
+  //   }
+  //   this.slides[2] = {
+  //     src: "../../../assets/living-room.jpg"
+  //   }
+  // }
+  //
+  // onItemChange($event: any): void {
+  //   console.log('Carousel onItemChange', $event);
+  // }
+
 }
