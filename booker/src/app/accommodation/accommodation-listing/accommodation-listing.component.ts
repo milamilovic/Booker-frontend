9ba76a199819917fb91818a531ddc08c331f86a8
milamilovic <milamilovic03@gmail.com>
2023-12-10 15:43:30 +0100

    filtering request sending, but incomplete

--- booker/src/app/accommodation/accommodation-listing/accommodation-listing.component.ts
+++ booker/src/app/accommodation/accommodation-listing/accommodation-listing.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {AccommodationListingDto} from "../accommodation/model/accommodation-listing.model";
 import {AccommodationService} from "../accommodation.service";
-import {ActivatedRoute} from "@angular/router";
-
+import {ActivatedRoute, Router} from "@angular/router";
+import {Filter} from "../accommodation/model/Filter";
 @Component({
   selector: 'app-accommodation-listing',
   templateUrl: './accommodation-listing.component.html',
@@ -16,7 +16,8 @@ export class AccommodationListingComponent implements OnInit {
   location: string = '';
   people: number = 0;
 
-  constructor(private service: AccommodationService, private route: ActivatedRoute) {
+  constructor(private service: AccommodationService, private route: ActivatedRoute,
+              private elemRef : ElementRef, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -39,5 +40,36 @@ export class AccommodationListingComponent implements OnInit {
 
   onAccommodationClick(accommodation: AccommodationListingDto) {
     this.clickedAcc = accommodation.title + " " + accommodation.id;
+  }
+
+  search(where_input: HTMLInputElement, from_date: HTMLInputElement, to_date: HTMLInputElement, people_input: HTMLInputElement) {
+    let location = where_input.value;
+    let start_date = from_date.value;
+    let end_date = to_date.value;
+    let people_search = Number(people_input.value);
+    let filters: Filter[] = [];
+    //TODO: make amenities dynamic (get amenity names from back)
+    let amenityNames: String[] = ['wifi', 'ac', 'goodLocation', 'cancellation'];
+    for(let amenity in amenityNames) {
+      let amenityCheck = this.elemRef.nativeElement.querySelector('amenity');
+      if(amenityCheck!=null && amenityCheck.isChecked()) {
+        filters.push({
+          "name": amenity,
+          "value": {
+            "checked": true
+          }
+        });
+      }
+    }
+    console.log("filterig!!!")
+    this.service.searchAndFilterAccommodations(start_date, end_date, location, people_search, filters).subscribe({
+      next: (data: AccommodationListingDto[]) => {
+        this.accommodations = data
+      },
+      error: (_) => {
+        console.log("Greska!")
+      }
+    })
+    //this.router.navigate(['/search', start_date.value, end_date.value, location.value, Number(people.value), '/filter']);
   }
 }
