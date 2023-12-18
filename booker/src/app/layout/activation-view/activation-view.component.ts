d079d2ad22f36cea7ce17347b6a9dac959403ab0
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2023-12-18 16:12:27 +0100

    fix: activation link recognition

--- booker/src/app/layout/activation-view/activation-view.component.ts
+++ booker/src/app/layout/activation-view/activation-view.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from "../../user/user.service";
 
 @Component({
@@ -9,10 +9,14 @@ import {UserService} from "../../user/user.service";
 })
 export class ActivationViewComponent {
   activationLink: string = "";
-  constructor(private route: ActivatedRoute, private userService: UserService) {}
+  constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     this.activationLink = this.route.snapshot.paramMap.get('activationLink')!;
     this.userService.activateProfile(this.activationLink);
+    setTimeout(() => {
+      // Navigate to the desired route after 5 seconds
+      this.router.navigate(['/home']); // Replace '/destination' with your actual destination route
+    }, 5000);
   }
 }
