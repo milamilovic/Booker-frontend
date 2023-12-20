f9d133b1738253c78c06d6bc2c7a2d456705a208
matejicdunja <dunja2002matejic@gmail.com>
2023-12-20 15:58:59 +0100

    dynamically inputs for update accommodation and better design

--- booker/src/app/accommodation/update-accommodation/update-accommodation.component.ts
+++ booker/src/app/accommodation/update-accommodation/update-accommodation.component.ts
@@ -1,9 +1,9 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {AccommodationViewDto} from "../accommodation/model/accommodation-view";
-import {AccommodationListingDto} from "../accommodation/model/accommodation-listing.model";
-import {Owner} from "../../user/owner-view/model/owner.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AccommodationService} from "../accommodation.service";
+import {AmenityDTO} from "../../amenity/AmenityDTO";
+import {AmenityService} from "../../amenity/amenity.service";
 
 @Component({
   selector: 'app-update-accommodation',
@@ -12,11 +12,14 @@ import {AccommodationService} from "../accommodation.service";
 })
 export class UpdateAccommodationComponent implements OnInit{
   accommodation!: AccommodationViewDto;
+  allAmenities: AmenityDTO[] = [];
+  amenitiesForAcc:AmenityDTO[] = [];
   id: number = 0;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private service: AccommodationService){}
+              private service: AccommodationService,
+              private amenityService: AmenityService){}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -24,6 +27,16 @@ export class UpdateAccommodationComponent implements OnInit{
       this.service.getAccommodation(this.id).subscribe({
         next: (data: AccommodationViewDto) => {
           this.accommodation = data;
+        }
+      });
+      this.amenityService.getAll().subscribe({
+        next: (all: AmenityDTO[]) => {
+          this.allAmenities = all;
+        }
+      });
+      this.amenityService.getAllForAccommodation(this.id).subscribe({
+        next:(all: AmenityDTO[]) => {
+          this.amenitiesForAcc = all;
         }
       })
     })
@@ -33,5 +46,29 @@ export class UpdateAccommodationComponent implements OnInit{
     // TODO set route for price & availability update component
     this.router.navigate(['']);
   }
+
+  autoSize(event: any) {
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+  }
+
+  isAvailable(amenity:AmenityDTO): boolean{
+    return this.amenitiesForAcc.some(item => item.name === amenity.name);
+  }
+
+  toggleAmenity(event: any, amenity: any) {
+    const isChecked = event.target.checked;
+
+  }
+
+  cancel(){
+    location.reload();
+  }
+
+  saveChanges(){
+
+  }
+
 
 }
