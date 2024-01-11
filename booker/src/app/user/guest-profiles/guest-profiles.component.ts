7e16a5a8dde6f7f8e9485f131793719d3015ee57
matejicdunja <dunja2002matejic@gmail.com>
2024-01-11 17:28:34 +0100

    guests table for owner - design and data

---
+++ booker/src/app/user/guest-profiles/guest-profiles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-profiles',
+  templateUrl: './guest-profiles.component.html',
+  styleUrls: ['./guest-profiles.component.css']
+})
+export class GuestProfilesComponent {
+
+}
